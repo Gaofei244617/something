b2e1b5f2be9c7146c938166a7eb958b9e516b2e4
GaoFei <gaofeiBUAA@outlook.com>
2024-11-29 22:53:23 +0800

    string split

---
+++ split.cpp
@@ -0,0 +1,19 @@
+#include <string>
+#include <vector>
+#include <iostream>
+#include <sstream>
+
+std::vector<std::string> split(const std::string &text, char delim)
+{
+    std::vector<std::string> tokens;
+    std::stringstream ss(text);
+    std::string item;
+    while (std::getline(ss, item, delim))
+    {
+        if (item.empty())
+        {
+            tokens.push_back(item);
+        }
+    }
+    return tokens;
+}
