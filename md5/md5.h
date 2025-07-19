41de155d27793f3b5291e8c0e3362cc93faed15d
GaoFei <gaofeiBUAA@outlook.com>
2025-07-19 21:40:03 +0800

    数据摘要

--- md5/md5.h
+++ md5/md5.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <string>
+#include <vector>
 #include <fstream>
 
 using Byte = unsigned char;
@@ -10,7 +11,8 @@ class MD5
 {
 public:
 	MD5();
-	std::string digest(const std::string& file);
+	std::string digest(const std::string& file);           // 文件摘要
+	std::string digest(const std::vector<uint8_t>& data);  // 数据摘要
 
 private:
 	void update(const Byte* input, size_t length);
@@ -23,4 +25,5 @@ private:
 	Byte m_buffer[64] = { 0 };   // input buffer
 };
 
+// 文件摘要
 std::string FileDigest(const std::string& file);
